5a7476e32ca502f90ce1cda275cd24d9d086f8c8
Chisom <anaelechisom16@gmail.com>
2024-01-28 15:12:10 +0100

    TDD tasks 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
-
+function reverseString(string){
+    let character = string.split('');
+    let reversedCharacters = character.reverse();
+    let reversedString = reversedCharacters.join('');
+    return reversedString;
+}
 // Do not edit below this line
 module.exports = reverseString;
