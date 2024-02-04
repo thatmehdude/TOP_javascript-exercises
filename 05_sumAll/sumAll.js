4367f4bac9e5b4744958ac85b455f2afac131f43
Chisom <anaelechisom16@gmail.com>
2024-02-04 21:59:15 +0100

    sum tdd task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+// const sumAll = function() {
 
-};
+// };
 
+function sumAll(num1, num2){
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+}
+    let result = 0;
+    for(i = num1; i <= num2; i++){
+        result += i;
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = sumAll;
